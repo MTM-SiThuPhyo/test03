c7e397abbe2f09415a9df731c466463cbab8d0da
MyatLay <scm.myatmon@gmail.com>
2019-12-17 16:12:34 +0630

    gg

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Group Git Testing</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatum blanditiis neque temporibus maiores nesciunt doloribus harum quis voluptas cumque dolores laboriosam, sit aliquid ea officiis deleniti accusantium qui amet?</p>
+</body>
+</html>
+
+
+
+
 <?php
     echo "Hello World";
 ?>
